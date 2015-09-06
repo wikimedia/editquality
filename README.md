2578dbb2c9b67fc7f65026ea8e9a3df63c35cb30
halfak <aaron.halfaker@gmail.com>
2015-09-06 14:38:29 -0500

    Adds python project stuff."

---
+++ README.md
@@ -0,0 +1,11 @@
+# Edit quality
+
+This library provides a set of utilities for building and maintaining
+edit quality prediction models.
+
+* **Installation:** ``pip install editquality``
+* **Repository:** https://github.com/wiki-ai/editquality
+* **License:** MIT
+
+## Author
+* Aaron Halfaker -- https://github.com/halfak
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def requirements(fname):
+    for line in open(os.path.join(os.path.dirname(__file__), fname)):
+        yield line.strip()
+
+setup(
+    name="editquality",
+    version="0.0.1",  # see editquality/__init__.py
+    author="Aaron Halfaker / Morten Warncke-Wang",
+    author_email="ahalfaker@wikimedia.org",
+    description="A library for performing automatic detection of the " +
+                "quality of Wikipedia edits.",
+    license="MIT",
+    url="https://github.com/wiki-ai/editquality",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'editquality=editquality.editquality:main'
+        ],
+    },
+    long_description=read('README.md'),
+    install_requires=requirements('requirements.txt'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering"
+    ],
+)
