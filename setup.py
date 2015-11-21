14f201cf645d3293360e073ff3cd547a4a2ddd8d
halfak <aaron.halfaker@gmail.com>
2015-11-21 11:33:52 -0600

    Increments version to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def requirements(fname):
 
 setup(
     name="editquality",
-    version="0.1.0",  # see editquality/__init__.py
+    version="0.1.1",  # see editquality/__init__.py
     author="Aaron Halfaker",
     author_email="ahalfaker@wikimedia.org",
     description="A library for performing automatic detection of the " +
