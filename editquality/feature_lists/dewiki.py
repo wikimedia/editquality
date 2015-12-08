ee903dbc2c21b1b9a8d069b7f9f462556a186bf7
halfak <aaron.halfaker@gmail.com>
2015-12-08 17:31:45 -0600

    Adds rules for separate user models and refactors features to split out protected_user features.

--- editquality/feature_lists/dewiki.py
+++ editquality/feature_lists/dewiki.py
@@ -1,7 +1,7 @@
 from revscoring.features.modifiers import log, max
 from revscoring.languages import german
 
-from . import enwiki, util
+from . import common, enwiki
 
 proportion_of_badwords_added = german.diff.badwords_added / \
                                max(german.diff.words_added, 1)
@@ -30,26 +30,39 @@ added_misspellings_ratio = proportion_of_misspellings_added / \
 added_informals_ratio = proportion_of_informals_added / \
                         max(proportion_of_informals, 0.01)
 
-damaging = util.no_lang_damaging + enwiki.badwords + enwiki.informals + [
-    log(german.diff.words_added + 1),
-    log(german.diff.words_removed + 1),
-    log(german.parent_revision.words + 1),
-    log(german.revision.words + 1),
+badwords = [
+    added_badwords_ratio,
     log(german.diff.badwords_added + 1),
     log(german.diff.badwords_removed + 1),
-    log(german.diff.informals_added + 1),
-    log(german.diff.informals_removed + 1),
-    log(german.diff.misspellings_added + 1),
-    log(german.diff.misspellings_removed + 1),
     proportion_of_badwords_added,
     proportion_of_badwords_removed,
-    proportion_of_informals_added,
-    proportion_of_informals_removed,
-    proportion_of_misspellings_added,
-    proportion_of_misspellings_removed,
-    added_badwords_ratio,
-    added_informals_ratio,
-    added_misspellings_ratio
 ]
 
+informals = [
+    added_informals_ratio,
+    log(german.diff.informals_added + 1),
+    log(german.diff.informals_removed + 1),
+    proportion_of_informals_added,
+    proportion_of_informals_removed
+]
+
+misspellings = [
+    added_misspellings_ratio,
+    log(german.diff.misspellings_added + 1),
+    log(german.diff.misspellings_removed + 1),
+    proportion_of_misspellings_added,
+    proportion_of_misspellings_removed
+]
+
+damaging = common.page + common.diff + common.user_rights + \
+           enwiki.badwords + enwiki.informals + \
+           badwords + informals + misspellings + \
+           [log(german.diff.words_added + 1),
+            log(german.diff.words_removed + 1),
+            log(german.parent_revision.words + 1)]
+damaging_user = damaging + common.protected_user
+
+reverted = damaging
+reverted_user = damaging_user
 goodfaith = damaging
+goodfaith_user = damaging_user
