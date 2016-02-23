aacc2613f1fcd849b54e0e80b080324c29ff8631
Ladsgroup <ladsgroup@ores-compute.revscoring.eqiad.wmflabs>
2016-02-23 21:02:14 +0000

    Add polish-related parts

---
+++ editquality/feature_lists/plwiki.py
@@ -0,0 +1,45 @@
+from revscoring.languages import polish
+
+from . import enwiki, mediawiki, wikipedia
+
+badwords = [
+    polish.badwords.revision.diff.match_delta_sum,
+    polish.badwords.revision.diff.match_delta_increase,
+    polish.badwords.revision.diff.match_delta_decrease,
+    polish.badwords.revision.diff.match_prop_delta_sum,
+    polish.badwords.revision.diff.match_prop_delta_increase,
+    polish.badwords.revision.diff.match_prop_delta_decrease
+]
+
+informals = [
+    polish.informals.revision.diff.match_delta_sum,
+    polish.informals.revision.diff.match_delta_increase,
+    polish.informals.revision.diff.match_delta_decrease,
+    polish.informals.revision.diff.match_prop_delta_sum,
+    polish.informals.revision.diff.match_prop_delta_increase,
+    polish.informals.revision.diff.match_prop_delta_decrease
+]
+
+dict_words = [
+    polish.dictionary.revision.diff.dict_word_delta_sum,
+    polish.dictionary.revision.diff.dict_word_delta_increase,
+    polish.dictionary.revision.diff.dict_word_delta_decrease,
+    polish.dictionary.revision.diff.dict_word_prop_delta_sum,
+    polish.dictionary.revision.diff.dict_word_prop_delta_increase,
+    polish.dictionary.revision.diff.dict_word_prop_delta_decrease,
+    polish.dictionary.revision.diff.non_dict_word_delta_sum,
+    polish.dictionary.revision.diff.non_dict_word_delta_increase,
+    polish.dictionary.revision.diff.non_dict_word_delta_decrease,
+    polish.dictionary.revision.diff.non_dict_word_prop_delta_sum,
+    polish.dictionary.revision.diff.non_dict_word_prop_delta_increase,
+    polish.dictionary.revision.diff.non_dict_word_prop_delta_decrease
+]
+
+damaging = wikipedia.page + \
+           mediawiki.wikitext + mediawiki.user_rights + \
+           mediawiki.protected_user + mediawiki.comment + \
+           badwords + informals + dict_words + \
+           enwiki.badwords + enwiki.informals
+
+reverted = damaging
+goodfaith = damaging
