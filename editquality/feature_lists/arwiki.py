c4a5f2453fd05e6a4ad3af9b0f46db48d1720821
Ladsgroup <ladsgroup@ores-compute.revscoring.eqiad.wmflabs>
2016-02-24 19:39:05 +0000

    Add Arabic

---
+++ editquality/feature_lists/arwiki.py
@@ -0,0 +1,45 @@
+from revscoring.languages import arabic
+
+from . import enwiki, mediawiki, wikipedia
+
+badwords = [
+    arabic.badwords.revision.diff.match_delta_sum,
+    arabic.badwords.revision.diff.match_delta_increase,
+    arabic.badwords.revision.diff.match_delta_decrease,
+    arabic.badwords.revision.diff.match_prop_delta_sum,
+    arabic.badwords.revision.diff.match_prop_delta_increase,
+    arabic.badwords.revision.diff.match_prop_delta_decrease
+]
+
+informals = [
+    arabic.informals.revision.diff.match_delta_sum,
+    arabic.informals.revision.diff.match_delta_increase,
+    arabic.informals.revision.diff.match_delta_decrease,
+    arabic.informals.revision.diff.match_prop_delta_sum,
+    arabic.informals.revision.diff.match_prop_delta_increase,
+    arabic.informals.revision.diff.match_prop_delta_decrease
+]
+
+dict_words = [
+    arabic.dictionary.revision.diff.dict_word_delta_sum,
+    arabic.dictionary.revision.diff.dict_word_delta_increase,
+    arabic.dictionary.revision.diff.dict_word_delta_decrease,
+    arabic.dictionary.revision.diff.dict_word_prop_delta_sum,
+    arabic.dictionary.revision.diff.dict_word_prop_delta_increase,
+    arabic.dictionary.revision.diff.dict_word_prop_delta_decrease,
+    arabic.dictionary.revision.diff.non_dict_word_delta_sum,
+    arabic.dictionary.revision.diff.non_dict_word_delta_increase,
+    arabic.dictionary.revision.diff.non_dict_word_delta_decrease,
+    arabic.dictionary.revision.diff.non_dict_word_prop_delta_sum,
+    arabic.dictionary.revision.diff.non_dict_word_prop_delta_increase,
+    arabic.dictionary.revision.diff.non_dict_word_prop_delta_decrease
+]
+
+damaging = wikipedia.page + \
+           mediawiki.wikitext + mediawiki.user_rights + \
+           mediawiki.protected_user + mediawiki.comment + \
+           badwords + informals + dict_words + \
+           enwiki.badwords + enwiki.informals
+
+reverted = damaging
+goodfaith = damaging
