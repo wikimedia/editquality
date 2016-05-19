16e46b0d49cd29cfa84cdf1706a134070ec9abc0
amir <ladsgroup@gmail.com>
2016-05-19 13:14:10 +0000

    Add svwiki reverted

---
+++ editquality/feature_lists/svwiki.py
@@ -0,0 +1,45 @@
+from revscoring.languages import swedish
+
+from . import enwiki, mediawiki, wikipedia, wikitext
+
+badwords = [
+    swedish.badwords.revision.diff.match_delta_sum,
+    swedish.badwords.revision.diff.match_delta_increase,
+    swedish.badwords.revision.diff.match_delta_decrease,
+    swedish.badwords.revision.diff.match_prop_delta_sum,
+    swedish.badwords.revision.diff.match_prop_delta_increase,
+    swedish.badwords.revision.diff.match_prop_delta_decrease
+]
+
+informals = [
+    swedish.informals.revision.diff.match_delta_sum,
+    swedish.informals.revision.diff.match_delta_increase,
+    swedish.informals.revision.diff.match_delta_decrease,
+    swedish.informals.revision.diff.match_prop_delta_sum,
+    swedish.informals.revision.diff.match_prop_delta_increase,
+    swedish.informals.revision.diff.match_prop_delta_decrease
+]
+
+dict_words = [
+    swedish.dictionary.revision.diff.dict_word_delta_sum,
+    swedish.dictionary.revision.diff.dict_word_delta_increase,
+    swedish.dictionary.revision.diff.dict_word_delta_decrease,
+    swedish.dictionary.revision.diff.dict_word_prop_delta_sum,
+    swedish.dictionary.revision.diff.dict_word_prop_delta_increase,
+    swedish.dictionary.revision.diff.dict_word_prop_delta_decrease,
+    swedish.dictionary.revision.diff.non_dict_word_delta_sum,
+    swedish.dictionary.revision.diff.non_dict_word_delta_increase,
+    swedish.dictionary.revision.diff.non_dict_word_delta_decrease,
+    swedish.dictionary.revision.diff.non_dict_word_prop_delta_sum,
+    swedish.dictionary.revision.diff.non_dict_word_prop_delta_increase,
+    swedish.dictionary.revision.diff.non_dict_word_prop_delta_decrease
+]
+
+damaging = wikipedia.page + \
+           wikitext.parent + wikitext.diff + mediawiki.user_rights + \
+           mediawiki.protected_user + mediawiki.comment + \
+           badwords + informals + dict_words + \
+           enwiki.badwords + enwiki.informals
+
+reverted = damaging
+goodfaith = damaging
