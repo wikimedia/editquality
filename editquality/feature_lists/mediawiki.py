30cdc42b068871672cdbb722ef9c8e155d3fdf29
Aaron Halfaker <aaron.halfaker@gmail.com>
2016-08-25 21:43:10 +0000

    Extends user group feature set.

--- editquality/feature_lists/mediawiki.py
+++ editquality/feature_lists/mediawiki.py
@@ -17,12 +17,22 @@ user_rights = [
     revision_oriented.revision.user.in_group(
         {'bot'}, name="revision.user.is_bot"),
     revision_oriented.revision.user.in_group(
-        {'checkuser', 'bureaucrat', 'oversight'},
+        {'checkuser', 'bureaucrat', 'oversight', 'steward'},
         name="revision.user.has_advanced_rights"),
     revision_oriented.revision.user.in_group(
         {'sysop'}, name="revision.user.is_admin"),
     revision_oriented.revision.user.in_group(
-        {'rollbacker', 'abusefilter', 'autopatrolled', 'reviewer'},
+        {'browsearchive', 'deletedhistory', 'interface-editor',
+         'noratelimit', 'accountcreator', 'massmessage-sender', 'templateeditor',
+         'autopatrolled', 'propertycreator', 'centralnoticeadmin'}, 
+        name="revision.user.is_trusted"),
+    revision_oriented.revision.user.in_group(
+        {'rollback', 'abusefilter', 'patroller', 
+         'reviewer', 'autoreview', 'autoreviewer', 'editor', 'autoeditor', 
+         'eliminator'},
+        name="revision.user.is_patroller"),
+    revision_oriented.revision.user.in_group(
+        {'import', 'filemover', 'suppressredirect'},
         name="revision.user.is_curator")
 ]
 
