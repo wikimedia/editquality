56391271be83e61200b2a7f66796ad5bac10412c
halfak <aaron.halfaker@gmail.com>
2017-01-07 12:37:38 -0600

    Adds about.py file.

--- setup.py
+++ setup.py
@@ -2,23 +2,26 @@ import os
 
 from setuptools import find_packages, setup
 
+about_path = os.path.join(os.path.dirname(__file__), "editquality/about.py")
+exec(compile(open(about_path).read(), about_path, "exec"))
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 def requirements(fname):
     for line in open(os.path.join(os.path.dirname(__file__), fname)):
         yield line.strip()
 
 setup(
-    name="editquality",
-    version="0.4.1",  # see editquality/__init__.py
-    author="Aaron Halfaker",
-    author_email="ahalfaker@wikimedia.org",
-    description="A library for performing automatic detection of the " +
-                "quality of Wikipedia edits.",
-    license="MIT",
-    url="https://github.com/wiki-ai/editquality",
+    name=__name__,  # noqa
+    version=__version__,  # noqa
+    author=__author__,  # noqa
+    author_email=__author_email__,  # noqa
+    description=__description__,  # noqa
+    url=__url__,  # noqa
+    license=read('LICENSE'),
     packages=find_packages(),
     entry_points={
         'console_scripts': [
