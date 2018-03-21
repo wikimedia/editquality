a44aa36131885f9c716ec7beeb1ed1e4d146ca0c
Adam Roses Wight <spam@ludd.net>
2018-03-21 14:54:22 -0500

    Simplify template (#144)
    This makes a slightly more readable and less fragile template, but it's still a chore to maintain.
    
    * Split autoreview files
    
    Note that having both the no_review and review rules doesn't cause the files to
    be automatically generated.  They must be a dependency of another, active rule
    in order for us to build them.
    
    * fetch_labels should handle missing labels
    
    fetch_labels will return partial or empty labels by omitting any missing
    labels.  merge_labels falls back to happy values when the observation was
    autolabeled as not needing review.  Observations needing review, with
    empty labels, are thrown out.
    
    * merge_labels handles nuances of human_labeled data
    
    This simplifies the Makefile.  merge_labels can merge partially labeled data,
    and will throw out unlabeled data flagged with needs_review.
    
    * Move more logic out of template
    
    No output Makefile changes.
    
    * Preserve a pass-through of needs_review human_labeled observations in merge_label
    
    * Make more linebreaks in the template
    
    * Autofill partially labeled, needs_review
    
    Guess damaging and goodfaith from inverse of one another.  This is an attempt
    to squeeze extra modeling value out of scarce training data, we'll measure to
    see what impact it has on model health.
    
    * [WIP] Rules to output model_info
    
    TODO:
    * How to call the rule any time a model is built?  Need to wrap rules?
    * mkdir for stats, .touchfile in git
    
    * Always prioritize autolabeled auto fields and human-labeled human fields
    
    Autolabeled data necessarily must be the newest source for autolabeling.
    Accidentally prioritizing auto fields in human-labeled observations was causing
    duplicate rows, as the autolabeled lines differ and may be routed to separate
    branches of the data pipeline simultaneously.

--- editquality/codegen/generate.py
+++ editquality/codegen/generate.py
@@ -6,9 +6,14 @@ from . import config
 def generate(config_path, templates_path):
     variables = config.load_config(config_path)
 
+    # TODO: map config into domain-specific preprocessed values
+
     env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(templates_path)
+        loader=jinja2.FileSystemLoader(templates_path),
+        lstrip_blocks=True,
+        trim_blocks=True
     )
 
+    # TODO: main input template should be configuration and parameter
     template = env.get_template("Makefile.j2")
     return template.render(variables) + "\n"
