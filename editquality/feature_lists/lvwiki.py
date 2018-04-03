82bb4d6ea29688ee1d6f52109eb9f6f4edd260f8
Aaron Halfaker <aaron.halfaker@gmail.com>
2018-04-02 19:37:00 -0500

    damaging and goodfaith models for arwiki/lvwiki (#147) * Adds feature_list for lvwiki * Adds config for damaging/goodfaith for lvwiki * Adds tuning reports and models for lvwiki damaging/goodfaith * Adds config, tuning, and models for arwiki damaging/goodfaith * Add updates to Makefile for arwiki and lvwiki Fixes a minor issue in deep_merge usage * Fixes some obvious mistakes in expected json output for config loader test. * Fixes the remaining issues with expected.json for test_config

---
+++ editquality/feature_lists/lvwiki.py
@@ -0,0 +1,45 @@
+from revscoring.languages import latvian
+
+from . import enwiki, mediawiki, wikipedia, wikitext
+
+badwords = [
+    latvian.badwords.revision.diff.match_delta_sum,
+    latvian.badwords.revision.diff.match_delta_increase,
+    latvian.badwords.revision.diff.match_delta_decrease,
+    latvian.badwords.revision.diff.match_prop_delta_sum,
+    latvian.badwords.revision.diff.match_prop_delta_increase,
+    latvian.badwords.revision.diff.match_prop_delta_decrease
+]
+
+informals = [
+    latvian.informals.revision.diff.match_delta_sum,
+    latvian.informals.revision.diff.match_delta_increase,
+    latvian.informals.revision.diff.match_delta_decrease,
+    latvian.informals.revision.diff.match_prop_delta_sum,
+    latvian.informals.revision.diff.match_prop_delta_increase,
+    latvian.informals.revision.diff.match_prop_delta_decrease
+]
+
+dict_words = [
+    latvian.dictionary.revision.diff.dict_word_delta_sum,
+    latvian.dictionary.revision.diff.dict_word_delta_increase,
+    latvian.dictionary.revision.diff.dict_word_delta_decrease,
+    latvian.dictionary.revision.diff.dict_word_prop_delta_sum,
+    latvian.dictionary.revision.diff.dict_word_prop_delta_increase,
+    latvian.dictionary.revision.diff.dict_word_prop_delta_decrease,
+    latvian.dictionary.revision.diff.non_dict_word_delta_sum,
+    latvian.dictionary.revision.diff.non_dict_word_delta_increase,
+    latvian.dictionary.revision.diff.non_dict_word_delta_decrease,
+    latvian.dictionary.revision.diff.non_dict_word_prop_delta_sum,
+    latvian.dictionary.revision.diff.non_dict_word_prop_delta_increase,
+    latvian.dictionary.revision.diff.non_dict_word_prop_delta_decrease
+]
+
+damaging = wikipedia.page + \
+    wikitext.parent + wikitext.diff + mediawiki.user_rights + \
+    mediawiki.protected_user + mediawiki.comment + \
+    badwords + informals + dict_words + \
+    enwiki.badwords + enwiki.informals
+
+reverted = damaging
+goodfaith = damaging
