64c2ee8d28ee5330f244b2b732137527310ce1a6
Amir Sarabadani <ladsgroup@gmail.com>
2018-08-22 16:53:42 +0200

    Update link to the github repo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This library provides a set of utilities for building and maintaining
 edit quality prediction models.
 
 * **Installation:** ``pip install editquality``
-* **Repository:** https://github.com/wiki-ai/editquality
+* **Repository:** https://github.com/wikimedia/editquality
 * **License:** MIT
 
 ## Author
--- editquality/about.py
+++ editquality/about.py
@@ -4,5 +4,5 @@ __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = "A library for performing automatic detection of the " + \
                   "quality of Wikipedia edits."
-__url__ = "https://github.com/wiki-ai/editquality"
+__url__ = "https://github.com/wikimedia/editquality"
 __license__ = "MIT"
